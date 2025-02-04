dfbee06b605b2b3af00a632cb7deb54c11fa5406
Bruno <66698615+BrunoAndrade1@users.noreply.github.com>
2025-02-04 17:04:29 -0300

    Update main.py

--- main.py
+++ main.py
@@ -46,10 +46,16 @@ with st.container():
 # Carregar os dados
 @st.cache_data
 def load_data():
+    # URL do arquivo bruto no GitHub
     url = "https://raw.githubusercontent.com/BrunoAndrade1/katia/main/raposo_nao_fatal.xlsx"
-    df = pd.read_excel(file_path, sheet_name="Planilha1")
+    
+    # Carregar o arquivo Excel diretamente do GitHub
+    df = pd.read_excel(url, sheet_name="Planilha1")
+    
+    # Converter colunas para datetime
     df["Data do Sinistro"] = pd.to_datetime(df["Data do Sinistro"], errors="coerce")
-    df["Hora do Sinistro"] = pd.to_datetime(df["Hora do Sinistro"], format='%H:%M:%S', errors="coerce").dt.hour
+    df["Hora do Sinistro"] = pd.to_datetime(df["Hora do Sinistro"], format="%H:%M:%S", errors="coerce").dt.hour
+    
     return df
 
 # Carregar e preparar os dados
